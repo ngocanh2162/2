ca48165ad9e890d3432d02dc9f053323ad474a10
ngocanh2162 <ngocanh2162@gmail.com>
2019-05-01 15:48:58 +0700

    .

--- run.py
+++ run.py
@@ -39,6 +39,7 @@ class NLP2(object):
         self.list2 = list2
         self.list3 = list3
 
+
     def builDictionary(self):
         list = self.list1
         l = self.list2
@@ -48,18 +49,19 @@ class NLP2(object):
         while i <= len(list) - 2:
             flag = True
             for j in range (l_leng):
-                if list[i] in l[j]:
-                    flag = False
-                    if j in (0,1,7):  # tên city, district, street thì ko thêm tiền tố từ
-                        str1 = ''.join(list[i+1]) 
-                    else:    
-                        str1 = ''
-                        str1 += list[i] + '_' + list[i+1]        
-                    if str1 not in f[j]:
-                        f[j].append(str1)
-                    if list[i+1] in f[l_leng]:
-                        f[l_leng].remove(list[i+1])
-                    break
+                for k in range (len(l[j])):
+                    if list[i] in l[j][k]:
+                        flag = False
+                        if j in (0,1,7):  # tên city, district, street thì ko thêm tiền tố từ
+                            str1 = ''.join(list[i+1]) 
+                        else:    
+                            str1 = ''
+                            str1 += list[i] + '_' + list[i+1]        
+                        if str1 not in f[j]:
+                            f[j].append(str1)
+                        if list[i+1] in f[l_leng]:
+                            f[l_leng].remove(list[i+1])
+                        break
             if flag == False:
                 i += 2
             else:
@@ -81,9 +83,15 @@ class NLP2(object):
                         f[l_leng].append(list[i])
         return f
     
+
+
+def writeListToTextFile(list, file):
+    for item in list:
+        file.write(str(item) + '\n')
+            
 f_ask = open(os.path.join("data","ask","ask.txt"),encoding="utf8")
 f_ask3 = open(os.path.join("data","ask","ask3.txt"),encoding="utf8")
-
+ask_file = [f_ask,f_ask3]
 dictionary_folder = 'E:\\2\\data\\dictionary' 
 dictionary_files = []
 # r=root, d=directories, f = dictionary_files
@@ -95,30 +103,24 @@ for i in range (0, len(dictionary_files)):
     open(dictionary_files[i], 'w',encoding="utf8")
 
 l = []
-l.append(['thành_phố', 'tỉnh'])
-l.append(['quận', 'thị_trấn', 'huyện', 'phường', 'khối', 'xóm', 'xã', 'thôn', 'làng'])
-l.append(['rạp_phim', 'công_viên', 'sở_thú', 'rạp_xiếc', 'quán', 'bảo_tàng','trung_tâm', 'sân'])
-l.append(['bến_xe', 'khách_sạn', 'chung_cư','lăng', 'tòa', 'tòa_nhà', 'ga', 'chợ', 'cầu', 'ngân_hàng', 'bệnh_viện', 'nghĩa_trang'])
-l.append(['hồ', 'sông','núi', 'đồi'])
-l.append(['nhà_hàng', 'quán_cà_phê', 'tiệm', 'quán', 'siêu_thị'])
-l.append(['trường','thư_viện', 'học_viện'])
-l.append(['đường', 'phố'])
-l.append(['đền', 'chùa', 'nhà_thờ', 'am', 'miếu'])
+l.append([['thành_phố'], ['tỉnh']])
+l.append([['quận'], ['thị_trấn'], ['huyện'], ['phường'], ['khối'], ['xóm'], ['xã'], ['thôn'], ['làng']])
+l.append([['rạp_phim'], ['công_viên'], ['sở_thú'], ['rạp_xiếc'], ['quán'], ['bảo_tàng'], ['trung_tâm'], ['sân']])
+l.append([['bến_xe'], ['khách_sạn'], ['nhà_nghỉ'], ['chung_cư'], ['lăng'], ['tòa'], ['tòa_nhà'], ['ga'], ['chợ'], ['cầu'], ['ngân_hàng'], ['bệnh_viện'], ['nghĩa_trang']])
+l.append([['hồ'], ['sông'], ['núi'], ['đồi']])
+l.append([['nhà_hàng'], ['quán_cà_phê'], ['tiệm'], ['quán'], ['siêu_thị']])
+l.append([['trường'], ['thư_viện'], ['học_viện']])
+l.append([['đường'], ['phố']])
+l.append([['đền'], ['chùa'], ['nhà_thờ'], ['am'], ['miếu']])
 
 f = []
 for i in range (len(l)+1):
     f.append(list())
 
-for line in f_ask3:
-    text = NLP(text=line).get_words()
-    f = list(NLP2(text, l, f).builDictionary())
-for line in f_ask:
-    text = NLP(text=line).get_words()
-    f = list(NLP2(text, l, f).builDictionary())
-
-def writeListToTextFile(list, file):
-    for item in list:
-        file.write(str(item) + '\n')
+for i in range (len(ask_file)):
+    for line in ask_file[i]:
+        text = NLP(text=line).get_words()
+        f = list(NLP2(text, l, f).builDictionary())
 
 for i in range (0, len(l) + 1):
     writeListToTextFile(f[i], open(dictionary_files[i], 'a',encoding="utf8"))
