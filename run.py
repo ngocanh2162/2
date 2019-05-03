16dedf709232c9ccbc55455e6fecad143ba50a74
ngocanh2162 <ngocanh2162@gmail.com>
2019-05-03 21:31:13 +0700

    .

--- run.py
+++ run.py
@@ -39,7 +39,6 @@ class NLP2(object):
         self.list2 = list2
         self.list3 = list3
 
-
     def builDictionary(self):
         list = self.list1
         l = self.list2
@@ -82,13 +81,17 @@ class NLP2(object):
                     if flag == True:
                         f[l_leng].append(list[i])
         return f
-    
 
+    def duplicateHandling(self):
+        f = self.list1
+        for i in range(len(f)+1):
+            for line in f[i]:
+                line = NLP(text=line).split_words()
 
 def writeListToTextFile(list, file):
     for item in list:
         file.write(str(item) + '\n')
-            
+
 f_ask = open(os.path.join("data","ask","ask.txt"),encoding="utf8")
 f_ask3 = open(os.path.join("data","ask","ask3.txt"),encoding="utf8")
 ask_file = [f_ask,f_ask3]
@@ -106,9 +109,9 @@ l = []
 l.append([['thành_phố'], ['tỉnh']])
 l.append([['quận'], ['thị_trấn'], ['huyện'], ['phường'], ['khối'], ['xóm'], ['xã'], ['thôn'], ['làng']])
 l.append([['rạp_phim'], ['công_viên'], ['sở_thú'], ['rạp_xiếc'], ['quán'], ['bảo_tàng'], ['trung_tâm'], ['sân']])
-l.append([['bến_xe'], ['khách_sạn'], ['nhà_nghỉ'], ['chung_cư'], ['lăng'], ['tòa'], ['tòa_nhà'], ['ga'], ['chợ'], ['cầu'], ['ngân_hàng'], ['bệnh_viện'], ['nghĩa_trang']])
-l.append([['hồ'], ['sông'], ['núi'], ['đồi']])
-l.append([['nhà_hàng'], ['quán_cà_phê'], ['tiệm'], ['quán'], ['siêu_thị']])
+l.append([['công_ty'], ['nhà_máy'], ['bến_xe'], ['khu_đô_thị'], ['khách_sạn'], ['nhà_nghỉ'], ['chung_cư'], ['lăng'], ['tòa'], ['tòa_nhà'], ['ga'], ['chợ'], ['cầu'], ['ngân_hàng'], ['bệnh_viện'], ['nghĩa_trang']])
+l.append([['hồ'], ['sông'], ['núi'], ['đồi'], ['đèo'], ['rừng'], ['suối'], ['biển'], ['vịnh']])
+l.append([['nhà_hàng'], ['quán_cà_phê'], ['tiệm'], ['quán'], ['siêu_thị'], ['cửa_hàng']])
 l.append([['trường'], ['thư_viện'], ['học_viện']])
 l.append([['đường'], ['phố']])
 l.append([['đền'], ['chùa'], ['nhà_thờ'], ['am'], ['miếu']])
@@ -116,11 +119,21 @@ l.append([['đền'], ['chùa'], ['nhà_thờ'], ['am'], ['miếu']])
 f = []
 for i in range (len(l)+1):
     f.append(list())
+h = []
+k = []
 
 for i in range (len(ask_file)):
     for line in ask_file[i]:
         text = NLP(text=line).get_words()
-        f = list(NLP2(text, l, f).builDictionary())
-
-for i in range (0, len(l) + 1):
-    writeListToTextFile(f[i], open(dictionary_files[i], 'a',encoding="utf8"))
+#         f = list(NLP2(text, l, f).builDictionary())
+        h.append(text)
+for i in range(len(h)):
+    for x in h[i]:
+        k.append(x)
+wordDict = dict.fromkeys(set(k), 0)
+for word in k:
+    wordDict[word]+=1
+print(wordDict)
+writeListToTextFile(k, open(dictionary_files[1], 'a',encoding="utf8"))
+# for i in range (0, len(f)):
+#     writeListToTextFile(f[i], open(dictionary_files[i], 'a',encoding="utf8"))
